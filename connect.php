54d14ae6b34878022b630ed281bf891b3cf48547
Mohammed Elshora <elshoram974@gmail.com>
2023-12-23 15:03:54 +0200

    refactor code

--- connect.php
+++ connect.php
@@ -12,7 +12,7 @@ $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8");
 try {
     $con = new PDO($dsn, $user, $pass, $options);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    include("./functions.php");
+    include("functions/functions.php");
 } catch (\Throwable $th) {
     // failureStatus($th->getMessage());
 }
