9231d12583780fcdf3478021b16f26e64a1559ea
Mohammed Elshora <elshoram974@gmail.com>
2024-01-26 08:24:00 +0200

    end new password endpoint for user

---
+++ auth/users/new_password.php
@@ -0,0 +1,40 @@
+<?php
+include("../../connect.php");
+include("../../core/class/app_user/app_user_model.php");
+
+
+$userId = postRequest('userId');
+$newPassword = postRequest('newPassword');
+
+$stmt = selectFromAppUserById($userId, $con);
+$count = $stmt->rowCount();
+
+if ($count > 0) {
+    $array = $stmt->fetch(PDO::FETCH_ASSOC);
+    $user =  User::fromArray($array);
+
+    $storedHash = $user->password;
+
+    if (!password_verify($newPassword, $storedHash)) {
+
+        if (strlen($newPassword) >= 8) {
+
+            $newHashPassword = makeHashPassword($newPassword);
+
+            $stmt = $con->prepare("UPDATE `app_users` SET `user_password`=? WHERE `user_id`=?");
+            $stmt->execute([$newHashPassword, $userId]);
+
+            $user->password = $newHashPassword;
+
+            $response = successState('user', $user->toArray());
+        } else {
+            $response = errorState(400, 'The password is very weak');
+        }
+    } else {
+        $response = errorState(400, 'You can\'t use the the same previous password');
+    }
+} else {
+    $response = errorState(409, 'The userId you entered does not exist');
+}
+
+echo json_encode($response);
