fcdca8ed0b7a6761be0772f519120bc31b866d56
Mohammed Elshora <elshoram974@gmail.com>
2024-01-26 08:34:25 +0200

    endpoint send_admin_verify_code

---
+++ auth/admins/send_admin_verify_code.php
@@ -0,0 +1,21 @@
+<?php
+
+include '../../connect.php';
+include("../../core/class/admin_model.php");
+
+$email = postRequest('email');
+
+$stmt = selectFromAdminsByEmail($email, $con);
+$count = $stmt->rowCount();
+
+if ($count > 0) {
+    $array = $stmt->fetch(PDO::FETCH_ASSOC);
+    $admin =  Admin::fromArray($array);
+
+    sendAdminVerifyEmail($admin->email);
+    $response = successState('admin', $admin->toArray());
+} else {
+    $response = errorState(401, 'The email you entered does not exist');
+}
+
+echo json_encode($response);
