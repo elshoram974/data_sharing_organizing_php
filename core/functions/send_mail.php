ea47ae3a74a15ce12de6cf60537d887435d31ce8
Mohammed Elshora <elshoram974@gmail.com>
2024-01-27 05:45:57 +0200

    add send mailer package

---
+++ core/functions/send_mail.php
@@ -0,0 +1,40 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../../libraries/phpmailer/Exception.php';
+require '../../libraries/phpmailer/PHPMailer.php';
+require '../../libraries/phpmailer/SMTP.php';
+
+function sendMail(string $toEmail, string $toName = '', string $subject, string $body): void
+{
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;
+        $mail->isSMTP();
+        $mail->Host       = 'mail.mrecode.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'sharikna@mrecode.com';
+        $mail->Password   = 'zGQ&]emImnQ=';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
+
+        //Recipients
+        $mail->setFrom('sharikna@mrecode.com', 'Sharikna');
+        $mail->addAddress($toEmail, $$toName);
+
+        //Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+    } catch (Exception $e) {
+        echo json_encode(errorState(500, "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"));
+        die;
+    }
+}
