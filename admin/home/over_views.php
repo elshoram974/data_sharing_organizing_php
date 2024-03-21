611720e19b97550517b4850b44dedbcb5d7b9eee
ali <alialhamoli@gmail.com>
2024-03-21 20:34:13 +0200

    fix over views api

--- admin/home/over_views.php
+++ admin/home/over_views.php
@@ -1,12 +1,18 @@
 <?php
 include "../../connect.php";
 
+$stmt = $con->prepare("SELECT count(user_id) AS `number_of_users` FROM app_users");
+$stmt->execute();
+$count=$stmt->fetch(PDO::FETCH_ASSOC);
+$response = successState("total_users_in_app", $count);
+
+
 $stmt=$con->prepare
 ("SELECT count(action_id) AS `number_of_users` FROM user_action_history 
 WHERE action_type='page_view' AND action_time >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ;");
 $stmt->execute();
 $count=$stmt->fetch(PDO::FETCH_ASSOC);
-$response = successState("visits_last_month", $count);
+$response["visits_last_month"] = $count;
 
 
 $stmt=$con->prepare
