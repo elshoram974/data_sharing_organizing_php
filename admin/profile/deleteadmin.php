74bfc0048daf64aa03d4457ca9587bbdbb09617e
ali <alialhamoli@gmail.com>
2024-03-23 06:20:55 +0200

    fix

--- admin/profile/deleteadmin.php
+++ admin/profile/deleteadmin.php
@@ -8,7 +8,7 @@ $deleteStmt = $con->prepare('DELETE FROM admins WHERE a_id  = ?');
 $deleteStmt->execute(array($admin_id));
 
 if(($deleteStmt->rowCount()) > 0){
-    $response = successState('response', ['massege' => 'User has been deleted successfully']);
+    $response = successState('response', ['massege' => 'Admin has been deleted successfully']);
 }else{
     $response = errorState(500, 'sql_query_failure', 'deleted failed try again.');
 }
