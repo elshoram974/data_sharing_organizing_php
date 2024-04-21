100d75cb6cad608e0a80b77386577e1ea35679a6
ali <alialhamoli@gmail.com>
2024-04-21 18:52:56 +0200

    fix home.php

--- user/home/home.php
+++ user/home/home.php
@@ -7,14 +7,21 @@ $action_type = "page_view";
 $action_details = "user is view home page";
 action($user_id, $action_type, $action_details, $con);
 
-$checkStmt = $con->prepare("SELECT user_password FROM app_users WHERE user_id = ?");
-$checkStmt->execute(array($user_id));
-$userpassword = $checkStmt->fetchAll(PDO::FETCH_ASSOC);
-if (!password_verify($user_password, $userpassword[0]['user_password'])) {
-    $response = errorState(401, 'authentication_error', 'password is changed');
-    echo json_encode($response);
-    exit;
+$check1Stmt = $con->prepare("SELECT user_provider FROM app_users WHERE user_id = ?");
+$check1Stmt->execute(array($user_id));
+$userprov = $check1Stmt->fetchAll(PDO::FETCH_ASSOC);
+if ($userprov == 'email_password') {
+    $check2Stmt = $con->prepare("SELECT user_password FROM app_users WHERE user_id = ?");
+    $check2Stmt->execute(array($user_id));
+    $userpassword = $check2Stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (!password_verify($user_password, $userpassword[0]['user_password'])) {
+        $response = errorState(401, 'authentication_error', 'password is changed');
+        echo json_encode($response);
+        exit;
+    }
 }
+
+
 
 $stateStmt = $con->prepare("SELECT user_status FROM app_users WHERE user_id = ?");
 $stateStmt->execute(array($user_id));
@@ -27,7 +34,7 @@ if ($userstatus[0]['user_status'] != "active") {
 
 
 
-$stmt = $con->prepare("SELECT `group_deails`.*
+$stmt = $con->prepare("SELECT `group_deails`.* , `group_members`.*
 FROM `group_deails`
 INNER JOIN `group_members` ON `group_deails`.`group_id` = `group_members`.`group_id`
 WHERE `group_members`.`member_id` = ?");
